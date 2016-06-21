8b3c3fb0077b91987fcd9156a83ecb13ccd4af4f
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-06-21 16:57:20 +0300

    Implemented ISupportRequiredService. Made unit tests running.

--- src/Castle.Windsor.MsDependencyInjection/ScopedWindsorServiceProvider.cs
+++ src/Castle.Windsor.MsDependencyInjection/ScopedWindsorServiceProvider.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Castle.Windsor.MsDependencyInjection
 {
     /// <summary>
     /// Implements <see cref="IServiceProvider"/>.
     /// </summary>
-    public class ScopedWindsorServiceProvider : IServiceProvider
+    public class ScopedWindsorServiceProvider : IServiceProvider, ISupportRequiredService
     {
         private readonly IWindsorContainer _container;
         private readonly MsLifetimeScope _ownMsLifetimeScope;
@@ -20,14 +21,30 @@ namespace Castle.Windsor.MsDependencyInjection
 
         public object GetService(Type serviceType)
         {
+            return GetServiceInternal(serviceType, true);
+        }
+
+        public object GetRequiredService(Type serviceType)
+        {
+            return GetServiceInternal(serviceType, false);
+        }
+
+        private object GetServiceInternal(Type serviceType, bool isOptional)
+        {
             using (MsLifetimeScope.Using(_ownMsLifetimeScope))
             {
                 // MS uses GetService<IEnumerable<TDesiredType>>() to get a collection.
                 // This must be resolved with IWindsorContainer.ResolveAll();
+
                 var typeInfo = serviceType.GetTypeInfo();
                 if (typeInfo.IsGenericType && serviceType.GetGenericTypeDefinition() == typeof(IEnumerable<>))
                 {
                     return _container.ResolveAll(typeInfo.GenericTypeArguments[0]);
+                }
+
+                if (!isOptional)
+                {
+                    return _container.Resolve(serviceType);
                 }
 
                 // A single service was requested.
@@ -35,6 +52,7 @@ namespace Castle.Windsor.MsDependencyInjection
                 // Microsoft.Extensions.DependencyInjection is built to handle optional registrations.
                 // However Castle Windsor throws a ComponentNotFoundException when a type wasn't registered.
                 // For this reason we have to manually check if the type exists in Windsor.
+
                 if (_container.Kernel.HasComponent(serviceType))
                 {
                     return _container.Resolve(serviceType);
